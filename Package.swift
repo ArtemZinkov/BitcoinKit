a94e49b902afd416d308c9ef7d701abb132e0d99
Artem Zinkov <spdzinkov@gmail.com>
2023-07-26 21:03:49 +0200

    updated code library

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
             targets: ["BitcoinKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/ArtemZinkov/BitcoinCore.git", .branch("master")),
         .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMinor(from: "1.2.1")),
-        .package(url: "https://github.com/horizontalsystems/Hodler.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/ArtemZinkov/Hodler.git", .branch("master")),
         .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMinor(from: "1.2.1")),
         .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
@@ -27,8 +27,8 @@ let package = Package(
             name: "BitcoinKit",
             dependencies: [
                 "BigInt",
-                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
-                .product(name: "Hodler", package: "Hodler.Swift"),
+                .product(name: "BitcoinCore", package: "BitcoinCore"),
+                .product(name: "Hodler", package: "Hodler"),
                 .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
                 .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                 .product(name: "HsToolKit", package: "HsToolKit.Swift"),
